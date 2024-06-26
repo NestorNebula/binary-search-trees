4fc822fc167499f08a55d66086b9edf57b326948
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-26 15:23:23 +0200

    Add createTree function
    The function creates the binary tree using recursion
    The function returns the root of the tree

--- tree.js
+++ tree.js
@@ -1,8 +1,22 @@
 import { sortArray } from './sortarray.js';
+import { Node } from './node.js';
 
 export function Tree(array) {
   const buildTree = (arr = array) => {
     const sortedArray = sortArray(arr);
+    console.log(sortedArray);
+    return createTree(sortedArray, 0, sortedArray.length - 1);
+  };
+
+  const createTree = (arr, start, end) => {
+    if (start > end) {
+      return null;
+    }
+    const middle = Math.floor((start + end) / 2);
+    const node = new Node(arr[middle]);
+    node.left = createTree(arr, start, middle - 1);
+    node.right = createTree(arr, middle + 1, end);
+    return node;
   };
 
   return {
